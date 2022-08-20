e46aa54f19debc6d69f208e62af1e08a95504484
Murat Tologlu <mtologlu@hotmail.com>
2022-08-20 05:43:20 +0300

    docs folder added for test

---
+++ docs/assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Test Post",
+        "excerpt":"This is a test post. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2020/04/26/test-post.html"
+      },{
+        "title": "Merhaba Dünya",
+        "excerpt":"This post not yet translated in English test Bir “Dummy Load” deneyimi 45 yıllık bir mühendis böyle bir hata yapar mı? Yapar! Faydalı Bağlantılar What are GitHub Pages Jekyll Quickstart Getting started with GitHub Pages Blogging with Jekyll on GitHub Pages Managing a custom domain for your GitHub Pages site...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2022/08/12/hello-world.html"
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2022/08/12/welcome-to-jekyll-en.html"
+      }]
---
+++ docs/tr/assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Deneme Postası",
+        "excerpt":"Bu bir deneme postasıdır. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2020/04/26/test-post.html"
+      },{
+        "title": "Merhaba Dünya",
+        "excerpt":"Yeniden merhaba. Bir “Dummy Load” deneyimi 45 yıllık bir mühendis böyle bir hata yapar mı? Yapar! test 3 Faydalı Bağlantılar What are GitHub Pages Jekyll Quickstart Getting started with GitHub Pages Blogging with Jekyll on GitHub Pages Managing a custom domain for your GitHub Pages site Markdown syntax Git Cheat...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2022/08/12/hello-world.html"
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/2022/08/12/welcome-to-jekyll-en.html"
+      }]
